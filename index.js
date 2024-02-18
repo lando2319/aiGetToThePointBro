e3952a16182e794a3eb83db2fdb5a70ad4d9c9af
lando2319 <mikepland@gmail.com>
2024-02-18 06:06:41 -0600

    add node-summarizer and implementation, french podcaster image in new dir images, updates README

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const whisper = require('whisper-node-anas23');
 
+let SummarizerManager = require("node-summarizer").SummarizerManager;
+
 (async () => {
     try {
         var options = {
             modelName:"base.en"
         };
 
-        const transcription = await whisper.whisper("/Users/mikeland/newDay/aiGetToThePointBro/audio/output2.wav", options);
+        const transcription = await whisper.whisper("/Users/mikeland/newDay/aiGetToThePointBro/audio/midjourney.wav", options);
 
-        console.log(transcription);
-        process.exit(0);
+        var fullTranscript = "";
+        transcription.forEach(workPkg => {
+            fullTranscript += workPkg.speech;
+        })
+
+        console.log("FULL TRANSCRIPT", fullTranscript);
+
+        let Summarizer = new SummarizerManager(fullTranscript, 2); 
+
+        // let summary = Summarizer.getSummaryByRank().then((summary_object)=>{
+        //     console.log("summary_object.summary");
+        //     console.log(summary_object.summary);
+        // })
+        let summary = Summarizer.getSummaryByFrequency().summary;
+
+        console.log("Summary");
+        console.log(summary);
     } catch (err) {
         console.log("ERROR", err);
         process.exit(1);
